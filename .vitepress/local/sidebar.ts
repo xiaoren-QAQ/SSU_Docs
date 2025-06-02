21c045fd7105db3b2436df1e2b49e3cd6139b0b0
YumeYuka <yumeyuka@oom-wg.dev>
2025-06-02 15:37:44 +0800

    update: refactor configuration files for improved structure, add page properties support, and enhance sidebar organization
    Signed-off-by: YumeYuka <Miao@YumeYuka.plus>

--- .vitepress/local/sidebar.ts
+++ .vitepress/local/sidebar.ts
@@ -1,34 +1,35 @@
 // sidebar.ts
 import type {DefaultTheme} from "vitepress";
 
-export const sidebar: DefaultTheme.Config["sidebar"] = [
-    {
-        text: "快速了解",
-        items: [
-            {text: "什么是SakitinSU", link: "/guide/what-is-sakitinsu"},
-            {text: "更新日志", link: "/changeLog/changelog"},
-        ],
-    },
-    {
-        text: "使用指南",
-        items: [
-            {text: "安装指南", link: "/guide/installation"},
-            {text: "常见问题", link: "/guide/faq"},
-        ],
-    },
-    {
-        text: "面向开发者",
-        items: [
-            {text: "模块开发", link: "/guide/installation"},
-            {text: "模块WebUI", link: "/guide/faq"},
-        ],
-    },
-    {
-        text: "关于SakitinSU",
-        items: [
-
-            {text: "SakitinSU的起源", link: "/origin"},
-            {text: "关于团队", link: "/about"},
-        ]
-    }
-];
+export const sidebar: DefaultTheme.Config["sidebar"] = {
+    "/": [
+        {
+            text: "快速了解",
+            items: [
+                {text: "什么是SakitinSU", link: "/guide/what-is-sakitinsu"},
+                {text: "更新日志", link: "/changeLog/changelog"},
+            ],
+        },
+        {
+            text: "使用指南",
+            items: [
+                {text: "安装指南", link: "/guide/installation"},
+                {text: "常见问题", link: "/guide/faq"},
+            ],
+        },
+        {
+            text: "面向开发者",
+            items: [
+                {text: "模块开发", link: "/development/module-development.md"},
+                {text: "模块WebUI", link: "/development/webui-module.md"},
+            ],
+        },
+        {
+            text: "关于SakitinSU",
+            items: [
+                {text: "SakitinSU的起源", link: "/about/origin"},
+                {text: "关于团队", link: "/about/about"},
+            ],
+        },
+    ],
+};
