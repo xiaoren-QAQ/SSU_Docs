3316391b4c4e60ab7b3abe272ce586e3fce4ed6a
天绾绮奈 <tianwan@oom-wg.dev>
2025-07-03 18:29:05 +0800

    Update index.js

--- src/public/functions/member/index.js
+++ src/public/functions/member/index.js
@@ -1,8 +1,4 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
-
-async function handleRequest(request) {
+export async function onRequest({request}) {
   const url = new URL(request.url);
   const path = url.pathname;
 
