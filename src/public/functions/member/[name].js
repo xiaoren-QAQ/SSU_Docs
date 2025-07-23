c39452713a477303f8262d61103014ea8bd3a444
白彩恋 <shiro@oom-wg.dev>
2025-07-23 14:41:46 +0800

    GitHub头像

--- src/public/functions/member/[name].js
+++ src/public/functions/member/[name].js
@@ -2,17 +2,5 @@ export async function onRequest(context) {
   const { request, params } = context;
   const name = params.name;
 
-  if (name !== "chenzyadb")
-    try {
-      const apiRes = await fetch("https://api.github.com/orgs/OOM-WG/members", {
-        headers: { "User-Agent": "GitHub" },
-      });
-      if (!apiRes.ok) return new Response(null, { status: 503 });
-      if (!(await apiRes.json()).some((member) => member.login === name))
-        return new Response(null, { status: 403 });
-    } catch (e) {
-      return new Response(null, { status: 503 });
-    }
-
-  return fetch(`https://github.com/${name}.png`);
+  return fetch(`https://ssu.oom-wg.dev/member/${name}`);
 }
